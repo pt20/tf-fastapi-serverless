f0d38d25e1c5b2541515844e571d2d97e682538b
pt20 <th.prayag@outlook.com>
2021-08-22 12:06:06 +0200

    add 2 new endpoints for getting model metadata

--- src/main.py
+++ src/main.py
@@ -2,27 +2,47 @@ import uvicorn
 from aiocogeo import COGReader
 from fastapi import FastAPI, HTTPException
 
-from inference import save_tiles_for_zoom
+from response_models import MlModelsConfig
+
+# from src.inference import save_tiles_for_zoom
+from utils import load_ml_config
 
 app = FastAPI(debug=True)
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome from the API"}
+    return {"message": "Sometimes the wheel turns slowly, but it turns."}
 
 
-@app.get("/windturbines/cog")
-async def get_windturbines(url: str):
-    print(url)
-    if url == "":
-        raise HTTPException(status_code=400, detail="cog url is needed")
+## deprecated in favour of AOI
+# @app.get("/windturbines/cog")
+# async def get_windturbines(url: str):
+#     if url == "":
+#         raise HTTPException(status_code=400, detail="cog url is required")
 
-    async with COGReader(url) as cog:
+#     async with COGReader(url) as cog:
 
-        fc = await save_tiles_for_zoom(cog, 17)
+#         fc = await save_tiles_for_zoom(cog, 17)
 
-    return fc
+#     return fc
+
+
+@app.get("/models")
+def list_models() -> MlModelsConfig:
+    return load_ml_config()
+
+
+## TODO: maybe use a uuid or something for model_id in the future
+@app.get("/models/{model_id}", description="Get a model metadata by id")
+def get_model_by_id(model_id: int) -> MlModelsConfig:
+    available_models = load_ml_config()
+    ids = [model.id for model in available_models.models]
+
+    if model_id not in ids:
+        raise HTTPException(404, detail="Model not found")
+
+    return [model for model in available_models.models if model.id == model_id][0]
 
 
 if __name__ == "__main__":
