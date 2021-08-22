462794eb5c375a900d2884c2a3391c2c61cfdf5a
pt20 <th.prayag@outlook.com>
2021-08-22 12:05:42 +0200

    add utility function for reading config file

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import os
+
+import yaml
+
+from response_models import MlModelsConfig
+
+file_path = os.path.join(os.path.dirname(__file__), "ml-config.yaml")
+
+
+def load_ml_config() -> MlModelsConfig:
+    with open(file_path, "r") as stream:
+        ml_config = yaml.safe_load(stream)
+
+    return MlModelsConfig(**ml_config)
+
+
+myconf = load_ml_config()
+
