7e392604b9dbc4434404572e9ff18e2c9f51beca
pt20 <th.prayag@outlook.com>
2021-08-28 21:28:42 +0200

    update shell script to install with poetry

--- get_started.sh
+++ get_started.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# export WORKON_HOME=~/Envs
 source $(which virtualenvwrapper.sh)
-# export WORKON_HOME=~/Envs
 
 VENV_NAME=$(basename "$PWD")
 echo $VENV_NAME
 
 mkvirtualenv -p $(which python3.8) $VENV_NAME
-
-# workon $VENV_NAME
 
 if ! command -v poetry &>/dev/null; then
     echo "poetry could not be found. See: https://python-poetry.org/docs/ for installation"
